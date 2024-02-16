f8a8b05e6f88ab4b93e47906ff421c1a01259df6
Hicham BOUCHIKHI <hicham.bouchikhi@adenes.eu>
2024-02-16 14:06:56 +0100

    Comment

--- src/ue_gvas_handler/src/bridge.rs
+++ src/ue_gvas_handler/src/bridge.rs
@@ -1,3 +1,16 @@
+/*
+This Rust library provides a C-compatible Foreign Function Interface (FFI) for handling Unreal Engine GVAS format data.
+
+Features:
+- Deserialization: Transforms Unreal Engine GVAS format data into a Rust structure via FFI.
+- Serialization: Converts a Rust structure back into Unreal Engine GVAS format via FFI.
+- Memory Management: Includes FFI functions to deallocate memory for Rust strings and vectors.
+
+Designed specifically for scenarios requiring interaction with Unreal Engine GVAS format data in a C-compatible FFI and Rust mixed environment.
+*/
+
+
+// Importing necessary libraries and modules
 extern crate libc;
 extern crate uesave;
 
@@ -6,18 +19,42 @@ use uesave::{Save, StructType, Types};
 use libc::{c_char, c_void};
 use std::ffi::{CStr, CString};
 
+// Defining a struct to hold key-value pairs
+// This struct is used in the deserialization process
 #[repr(C)]
 pub struct KeyValuePair {
-    key: *const c_char,
-    value: *const c_char,
+    key: *const c_char,   // Key in the key-value pair
+    value: *const c_char, // Value in the key-value pair
 }
 
+// Defining a struct to hold result data
+// This struct is used in the serialization process
 #[repr(C)]
 pub struct Result {
-    ptr: *const c_void,
-    size: usize,
+    ptr: *const c_void, // Pointer to the serialized data
+    size: usize,        // Size of the serialized data
 }
 
+
+/*
+Function to deserialize data from Unreal Engine GVAS format to a Rust structure.
+This function takes a buffer, its size, a map, and the map's length as parameters, and returns a pointer to a character. 
+The buffer should contain the data in Unreal Engine GVAS format. The map should contain key-value pairs that represent the structure of the data.
+
+The function first converts the raw buffer and map into Rust data structures. 
+It then uses these data structures to deserialize the buffer into a Rust structure. The deserialized data is then used by the EA Save library.
+
+The function returns a pointer to a C string that represents the deserialized data. 
+This C string can be used in C code and should be deallocated using the `free_rust_string` function when it is no longer needed.
+Parameters:
+- buffer: A pointer to the data in Unreal Engine GVAS format.
+- size: The size of the buffer.
+- map: A pointer to an array of KeyValuePair structures that represent the structure of the data.
+- map_length: The number of elements in the map.
+
+Returns:
+- A pointer to a C string that represents the deserialized data.
+*/
 #[no_mangle]
 pub extern "C" fn deserialize(buffer: *const c_void, size: libc::size_t, map: *const KeyValuePair, map_length: libc::size_t) -> *mut c_char {
     // Convert the raw buffer to a byte slice
@@ -52,6 +89,25 @@ pub extern "C" fn deserialize(buffer: *const c_void, size: libc::size_t, map: *c
     c_str.into_raw()
 }
 
+/*
+Function to serialize data from a json string to Unreal Engine GVAS format.
+This function takes a pointer to a JSON string and a pointer to its size as parameters, 
+and returns a pointer to void. 
+The JSON string should represent a Rust structure that is compatible with the Unreal Engine GVAS format.
+
+The function first converts the JSON string into a Rust `Save` structure using the `serde_json::from_str` function. 
+It then serializes this `Save` structure into a byte vector in the Unreal Engine GVAS format.
+
+The function returns a pointer to the serialized data. 
+This pointer can be used in C code and should be deallocated using the `free_rust_vec` function when it is no longer needed.
+
+Parameters:
+- json: A pointer to a Unreal Engine JSON string that represents a Rust structure.
+- size: A pointer to a variable where the size of the serialized data will be stored.
+
+Returns:
+- A pointer to the serialized data in the Unreal Engine GVAS format.
+*/
 #[no_mangle]
 pub extern "C" fn serialize(json: *const c_char,  size: *mut usize) -> *mut c_void {
     let json = unsafe { CStr::from_ptr(json).to_string_lossy().into_owned() };
@@ -70,6 +126,8 @@ pub extern "C" fn serialize(json: *const c_char,  size: *mut usize) -> *mut c_vo
     ptr as *mut c_void
 }
 
+// Function to free memory allocated for a Rust string
+// This function is used to free the memory allocated for a Rust string.
 #[no_mangle]
 pub extern "C" fn free_rust_string(s: *mut c_char) {
     unsafe {
@@ -78,6 +136,8 @@ pub extern "C" fn free_rust_string(s: *mut c_char) {
     };
 }
 
+// Function to free memory allocated for a Rust vector
+// This function is used to free the memory allocated for a Rust vector.
 #[no_mangle]
 pub extern "C" fn free_rust_vec(p: *mut c_void) {
     unsafe {
