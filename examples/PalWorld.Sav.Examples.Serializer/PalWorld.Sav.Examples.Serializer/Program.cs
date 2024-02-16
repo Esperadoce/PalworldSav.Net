7f8dbe05cfd564912755527618127211adfbf4b7
Esperadoce <hbouchikhi@outlook.fr>
2024-02-16 20:14:12 +0100

    Add Serilizer Example

---
+++ examples/PalWorld.Sav.Examples.Serializer/PalWorld.Sav.Examples.Serializer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.IO;
+using PalWorld.Sav.Serializer;
+
+
+string jsonFilePath = "example.json";
+string content = File.ReadAllText(jsonFilePath);
+var data = await UeSave.SerializeAsync(content);
+
+string filePath = $"example.{Guid.NewGuid()}.sav";
+await SavFileHandler.WriteSavAsync(new SaveFileFormatWriter(filePath, data), true);
+
